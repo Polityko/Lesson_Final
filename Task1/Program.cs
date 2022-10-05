42770ff22d23fee3b6d448fdf2c83f607eb95cc8
Olga <politykoolga@outlook.com>
2022-10-05 22:07:59 +0300

    Program

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] A = new string[10];
+Console.WriteLine("Введите масcив строк через пробел: ");
+A = Console.ReadLine().Split(" ");
+string[] resultat = new string[A .Length];
+int i = 0;
+foreach (string elementfromA in A)
+{
+    if (elementfromA.Length <= 3)
+    {
+        resultat[i] = elementfromA;
+        i++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, resultat, 0, i));
